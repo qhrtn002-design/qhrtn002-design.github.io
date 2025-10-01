750de49604f92fcd3fdec1b8d17a829c3e13ce93
qhrtn002-design <qhrtn002@gmail.com>
2025-10-01 11:42:56 +0900

    step04

--- 251001_phase01/server.js
+++ 251001_phase01/server.js
@@ -1,12 +1,10 @@
-// server.js
-// es-module -> import, commonjs -> require
-const express = require("express"); // express 안에 있는 이미 구현되어 있는 코드들을 express 객체 형태로 불러오겠다
+const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
-const { createClient } = require("@supabase/supabase-js");
+const { createClient } = require("@supabase/supabase-js"); // 구조분해 할당
 
-dotenv.config();
-
+dotenv.config(); // .env -> KEY => SUPABASE_KEY
+// NODE -> process.env (환경변수) // cf. env file
 
 // const supabaseKey = process.env.SUPABASE_KEY;
 // const supabaseUrl = process.env.SUPABASE_URL;
@@ -15,24 +13,23 @@ console.log("supabaseKey", supabaseKey); // 확인 (npm run dev)
 console.log("supabaseUrl", supabaseUrl);
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-const app = express(); // () -> 호출해서 사용하겠다
-// 포트 -> 컴퓨터 서비스가 1개만 있는게 아님. email, db, server 1, server 2...
-// 1 ~ 2xxxx. => 이 번호로 오세요...
-const port = 3000; // cra. next -> express. / 5173.
-// localhost -> 3000. / 5500? <-> 구분해주는 의미.
+const app = express();
+const port = 3000;
 
 app.use(cors());
 
-// get, post...
-// app.방식(접속경로, 핸들러)
-// localhost:3000/
 app.get("/", (req, res) => {
-  // req -> request -> 전달 받은 데이터나 요청사항
-  // res -> response -> 응답할 내용/방식을 담은 객체
   res.send("bye");
 });
 
-// DOM listener / server '대기' -> 특정한 요청. -> 응답.
+app.get("/plans", async (req, res) => {
+  const { data, error } = await supabase.from("tour_plan").select("*");
+  if (error) {
+    return res.status(400).json({ error: error.message });
+  }
+  res.json(data);
+});
+
 app.listen(port, () => {
   console.log(`서버가 ${port}번 포트로 실행 중입니다.`);
 });
