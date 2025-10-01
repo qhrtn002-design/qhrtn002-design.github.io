ae8ac0000ff174c80386d61c233d9f044fc5ddcb
qhrtn002-design <qhrtn002@gmail.com>
2025-10-01 12:43:57 +0900

    step06

--- 251001_phase01/server.js
+++ 251001_phase01/server.js
@@ -17,8 +17,7 @@ const app = express();
 const port = 3000;
 
 app.use(cors());
-app.use(express.json());
-
+app.use(express.json()); // req.body -> json.
 
 app.get("/", (req, res) => {
   res.send("bye");
@@ -33,13 +32,25 @@ app.get("/plans", async (req, res) => {
 });
 
 app.post("/plans", async (req, res) => {
-    const plan = req.body;
-    const {error} = await supabase.from("tour_plan").insert(plan);
-    if (error) {
+  const plan = req.body;
+  const { error } = await supabase.from("tour_plan").insert(plan);
+  if (error) {
     return res.status(400).json({ error: error.message });
   }
   res.status(201).json();
-  })
+});
+
+app.delete("/plans", async (req, res) => {
+  const { planId } = req.body;
+  const { error } = await supabase
+    .from("tour_plan") // table
+    .delete() // 삭제
+    .eq("id", planId); // eq = equal = id가 planId
+  if (error) {
+    return res.status(400).json({ error: error.message });
+  }
+  res.status(204).json(); // noContent
+});
 
 app.listen(port, () => {
   console.log(`서버가 ${port}번 포트로 실행 중입니다.`);
