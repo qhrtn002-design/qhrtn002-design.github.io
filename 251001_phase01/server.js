f2f2aaa3514f4cbd2f0fa3a40fb9521771f5fda2
qhrtn002-design <qhrtn002@gmail.com>
2025-10-01 10:19:23 +0900

    step02

--- 251001_phase01/server.js
+++ 251001_phase01/server.js
@@ -1,12 +1,16 @@
 // server.js
 // es-module -> import, commonjs -> require
 const express = require("express"); // express 안에 있는 이미 구현되어 있는 코드들을 express 객체 형태로 불러오겠다
+const cors = require("cors");
+
 
 const app = express(); // () -> 호출해서 사용하겠다
 // 포트 -> 컴퓨터 서비스가 1개만 있는게 아님. email, db, server 1, server 2...
 // 1 ~ 2xxxx. => 이 번호로 오세요...
 const port = 3000; // cra. next -> express. / 5173.
 // localhost -> 3000. / 5500? <-> 구분해주는 의미.
+
+app.use(cors());
 
 // get, post...
 // app.방식(접속경로, 핸들러)
